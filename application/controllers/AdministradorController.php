7eb8b595f4a8b6eaeb56294f0b4d23284262a559
Ariel May <aemd199711@gmail.com>
2020-04-20 22:06:42 -0500

    Permisos

--- application/controllers/AdministradorController.php
+++ application/controllers/AdministradorController.php
@@ -9,6 +9,10 @@ class AdministradorController extends RestController {
     public function __construct() {
         parent::__construct();
 
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        
         $this->load->model($this->modelName);
         $this->peticion = new Peticion();
     }
